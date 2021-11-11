f1e2d890cad75d5c80d3c979b49a66db53603076
Vidottizzz <vidottizzz@gmail.com>
2021-11-11 13:18:48 -0300

    Criando a section Sobre e serviços com HTML/CSS e utilizando JS para auxiliar no design ao rolar a tela

--- main.js
+++ main.js
@@ -13,3 +13,16 @@ for (const title of titles) {
         nav.classList.remove('show')
     })
 }
+
+// mudar o header da pagina quando der scroll
+
+const header = document.querySelector('#header');
+const navHeight = header.offsetHeight;
+
+window.addEventListener('scroll', ()=> {
+    if(window.scrollY >= navHeight) {
+    header.classList.add('scroll');
+    } else {
+    header.classList.remove('scroll');
+    }
+})
